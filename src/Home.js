531a309c0d2f16b913fa60f9fe64040e72d073ac
Anup Kumar <anup.raj1989@gmail.com>
2022-03-11 15:01:12 +0530

    feat: style the navbar and container with bootstrap

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,13 @@
 const Home = () => {
   return (
     <>
-      <div className="container">Home Works!!</div>
+      <div className="container">
+        <h1>Hello, world!</h1>
+        <p>
+          This is a simple hero unit, a simple jumbotron-style component for
+          calling extra attention to featured content or information.
+        </p>
+      </div>
     </>
   );
 };
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,8 +1,29 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
 const NavBar = () => {
   return (
-    <>
-      <div>NavBar</div>
-    </>
+    <React.Fragment>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <a className="navbar-brand ml-2">
+          <img
+            src="logo512.png"
+            width="30"
+            height="30"
+            className="d-inline-block align-top mr-2"
+            alt=""
+          />
+          Social Media App
+        </a>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/" exact>
+              Home
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </React.Fragment>
   );
 };
 
